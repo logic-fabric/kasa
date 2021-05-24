9cbb8bb0cff18c12f24925fd8f755ef11ea91fdf
logic-fabric <lowic@logic-fabric.fr>
2021-05-24 11:22:58 +0200

    feature: adapt CRA initial setup to Kasa project

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
+import App from "./components/App";
+
+test("Renders 'learn react' link", () => {
   render(<App />);
+
   const linkElement = screen.getByText(/learn react/i);
+
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import logo from "../logo.svg";
+import "../App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="Kasa" />
+      </div>
+    );
+  }
+}
+
+export default App;
