cda3df3c150bdf67bcf12bd1d4673acdc494b6b7
logic-fabric <lowic@logic-fabric.fr>
2021-07-09 12:12:50 +0200

    feature: complete and style Error404 page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../css/Error404.css";
 
@@ -6,7 +7,13 @@ class Error404 extends React.Component {
   render() {
     return (
       <div className="Error404">
-        <h1>404</h1>
+        <p className="Error404__status-code">404</p>
+        <p className="Error404__text">
+          Oups ! La page que vous demandez n'existe pas.
+        </p>
+        <Link className="Error404__link-to-home" to="/">
+          Retourner sur la page d'accueil
+        </Link>
       </div>
     );
   }
