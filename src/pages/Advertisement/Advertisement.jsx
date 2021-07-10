cd4806649d9d427ca6d955b53356350ed94e0339
logic-fabric <lowic@logic-fabric.fr>
2021-07-10 09:57:44 +0200

    feature: Advertisement page basic HTML with ad data

--- src/pages/Advertisement/Advertisement.jsx
+++ src/pages/Advertisement/Advertisement.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Error404 from "../Error404/Error404";
+import Collapse from "../../components/Collapse/Collapse";
 import "./Advertisement.css";
 
 class Advertisement extends React.Component {
@@ -18,9 +19,30 @@ class Advertisement extends React.Component {
       return <Error404 />;
     }
 
+    const ad = advertisementToDisplay;
+
     return (
       <main className="Advertisement">
-        <h1>Hello location {id} !</h1>
+        <div>[ CAROUSEL - COMING SOON ]</div>
+        <section>
+          <div>
+            <h1>{ad.title} !</h1>
+            <h2>{ad.location}</h2>
+            <div>[ TAGS - COMING SOON ]</div>
+          </div>
+          <div>
+            <p>{ad.host.name}</p>
+            <img src={ad.host.picture} alt={ad.host.name} />
+            <div>[ RATING - COMING SOON ]</div>
+          </div>
+          <div>
+            <Collapse title="Description" text={ad.description} />
+            <Collapse
+              title="Êquipements"
+              text={`${ad.equipments.join(", ")}`}
+            />
+          </div>
+        </section>
       </main>
     );
   }
