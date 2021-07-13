cb9012bdcc451cb805b66b29d5304bbd595a9cda
logic-fabric <lowic@logic-fabric.fr>
2021-07-13 18:26:17 +0200

    feature: add a Link to Home on Footer logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Footer.css";
 import logoWhite from "../../logo-white.svg";
@@ -7,7 +8,9 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <img src={logoWhite} alt="Kasa" />
+        <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+          <img src={logoWhite} alt="Kasa" />
+        </Link>
         <p>&copy; 2021 Kasa. Tous droits réservés</p>
       </footer>
     );
