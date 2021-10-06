930f471518b9ba58869875e2fc590e2e5a3ada8f
logic-fabric <lowic@logic-fabric.fr>
2021-10-06 15:30:05 +0200

    refactor: remove useless try/catch in dataFetcher.js

--- src/data/dataFetcher.js
+++ src/data/dataFetcher.js
@@ -17,40 +17,34 @@ export class DataFetcher {
    * @returns {PropertyAdvertisementsList}
    */
   async fetchSource() {
-    try {
-      let response = await fetch(this._dataSource);
+    let response = await fetch(this._dataSource);
 
-      if (response.ok) {
-        let data = await response.json();
+    if (response.ok) {
+      let data = await response.json();
 
-        const advertisementInstances = [];
+      const advertisementInstances = [];
 
-        for (let advertisement of data) {
-          const advertisementInstance = new PropertyAdvertisement(
-            advertisement.id,
-            advertisement.title,
-            advertisement.cover,
-            advertisement.pictures,
-            advertisement.description,
-            advertisement.host,
-            advertisement.rating,
-            advertisement.location,
-            advertisement.equipments,
-            advertisement.tags
-          );
-
-          advertisementInstances.push(advertisementInstance);
-        }
-
-        return new PropertyAdvertisementsList(advertisementInstances);
-      } else {
-        console.error(
-          `HTTP-Error-${response.status} while fetching ${this._dataSource}`
+      for (let advertisement of data) {
+        const advertisementInstance = new PropertyAdvertisement(
+          advertisement.id,
+          advertisement.title,
+          advertisement.cover,
+          advertisement.pictures,
+          advertisement.description,
+          advertisement.host,
+          advertisement.rating,
+          advertisement.location,
+          advertisement.equipments,
+          advertisement.tags
         );
+
+        advertisementInstances.push(advertisementInstance);
       }
-    } catch (err) {
+
+      return new PropertyAdvertisementsList(advertisementInstances);
+    } else {
       console.error(
-        `An error as occured while fetching ${this._dataSource} : ${err}`
+        `HTTP-Error-${response.status} while fetching ${this._dataSource}`
       );
     }
   }
